510334d2e0bd4e94876a9e33f22be432b8a40e80
Jahongir <jahongirnagmatov@gmail.com>
2021-06-28 13:26:44 +0500

    Create Users model

--- admin/telegram_bot/models.py
+++ admin/telegram_bot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=20, null=True)
+    last_name = models.CharField(max_length=20, null=True)
+    phone_number = models.IntegerField(max_length=12)
+    username = models.CharField(max_length=20, null=True)
+
