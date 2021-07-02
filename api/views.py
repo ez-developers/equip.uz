acc36ce86f1cc9dc6c2ac6ede779cde8ef963bbc
Jahongir <jahongirnagmatov@gmail.com>
2021-07-02 17:42:37 +0500

    fix bugs and add PUT and DELETE method for each product path

--- api/views.py
+++ api/views.py
@@ -22,26 +22,45 @@ def getUser(request, pk):
 
 
 
-@api_view(['GET',])
-@permission_classes([IsAuthenticated])
-def getProducts(request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)    
-
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def getProduct(request, pk):
+def getProducts(request):
     if request.method == 'GET':
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product, many=False)
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
     elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
+        serializer = ProductSerializer(data=request.data)  
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def getProduct(request, pk):
+
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.status.HTTP_404_NOT_FOUND)
+
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET',])
