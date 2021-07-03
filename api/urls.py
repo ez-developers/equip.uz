c1a3b7a0aa5556935d0017373fbbe50c14ef71f1
Jahongir <jahongirnagmatov@gmail.com>
2021-07-03 12:02:26 +0500

    Rewrite api using class based views (only for products model)

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('users/', views.getUsers, name="users"),
     path('users/<pk>', views.getUser, name="user"),
-    path('products/', views.getProducts, name="products"),
+    path('products/', views.getProducts.as_view(), name="products"),
     path('products/<pk>', views.getProduct, name="product"),
     path('categories/', views.getCategories, name="categories"),
     path('categories/<pk>', views.getCategory, name="category"),
--- api/views.py
+++ api/views.py
@@ -5,6 +5,23 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import UserSerializer, CategorySerializer, ProductSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+
+class getProducts(APIView):
+    
+    def get(self, request):
+       products = Product.objects.all()
+       serializer = ProductSerializer(products, many=True)
+       return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET',])
 @permission_classes([IsAuthenticated])
@@ -22,20 +39,20 @@ def getUser(request, pk):
 
 
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def getProducts(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def getProducts(request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)  
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)  
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
