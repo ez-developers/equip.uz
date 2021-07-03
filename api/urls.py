808c040486b284d143fcf2a2f74c971ea3fedbc4
Jahongir <jahongirnagmatov@gmail.com>
2021-07-03 12:21:20 +0500

    class based views api in productDetail

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('users/', views.getUsers, name="users"),
     path('users/<pk>', views.getUser, name="user"),
-    path('products/', views.getProducts.as_view(), name="products"),
-    path('products/<pk>', views.getProduct, name="product"),
+    path('products/', views.productsList.as_view(), name="products"),
+    path('products/<pk>', views.productDetail.as_view(), name="product"),
     path('categories/', views.getCategories, name="categories"),
     path('categories/<pk>', views.getCategory, name="category"),
 ]
--- api/views.py
+++ api/views.py
@@ -8,7 +8,8 @@ from .serializers import UserSerializer, CategorySerializer, ProductSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 
-class getProducts(APIView):
+@permission_classes([IsAuthenticated])
+class productsList(APIView):
     
     def get(self, request):
        products = Product.objects.all()
@@ -54,8 +55,38 @@ def getUser(request, pk):
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
 
-@api_view(['GET', 'PUT', 'DELETE'])
+
 @permission_classes([IsAuthenticated])
+class productDetail(APIView):
+    
+    def get_object(self, pk):
+        try: 
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):
+        product=self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
 def getProduct(request, pk):
 
     try:
