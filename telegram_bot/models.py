b1595bb5339d9c6ee4e4a48307690d156e97f669
Jahongir <jahongirnagmatov@gmail.com>
2021-07-03 12:00:21 +0500

    add verbose name to models

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    class Meta:
+        verbose_name_plural = "Bot Users"
+        verbose_name = "Bot User"
+
     user_id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=50, null=True, blank=False)
     last_name = models.CharField(max_length=50, null=True, blank=False)
@@ -10,21 +14,28 @@ class User(models.Model):
     language_code = models.CharField(max_length=8, null=True, blank=True)
     joined_at = models.DateTimeField(auto_now_add=True)
 
-class Category(models.Model):
-
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+        verbose_name = "Category"
+
 
     name = models.CharField(max_length=255, null=True)
 
-class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 
