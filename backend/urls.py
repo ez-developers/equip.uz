c40f9cac6589852a93a79d94958349b85d0b91e5
Jahongir <jahongirnagmatov@gmail.com>
2021-07-05 20:32:02 +0500

    redirect to admin

--- backend/urls.py
+++ backend/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url="/admin")),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls'))
