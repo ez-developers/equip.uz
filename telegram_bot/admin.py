37e693b512b0dbbef5fb8952c871f24116227c30
johndev <jahongirnagmatov@gmail.com>
2021-07-08 12:35:34 +0500

    add filter to Product category

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, Category, Product
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -8,21 +9,14 @@ from .models import User, Category, Product
 class UserAdmin(admin.ModelAdmin):
     list_display = ("user_id", "first_name", "last_name")
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_edit_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request):
-        return False
-
-
 @admin.register(Category)
-class UserAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Product)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class ProductAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'colored_price', 'colored_category')
+    list_filter = ("category", )
+
