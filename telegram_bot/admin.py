592ffc439b877856465f2d5e7216b80aed7d3026
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-07-08 08:35:16 +0500

    admin interface for users model

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_edit_permission(self, request):
+    def has_change_permission(self, request):
         return False
 
     def has_delete_permission(self, request):
