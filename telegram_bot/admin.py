7fb3564af04d453bdd6195f9a57e6ace67a7fa9c
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-07-10 18:54:37 +0500

    admin interface, user model updated

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -12,9 +12,18 @@ admin.site.site_header = "Администрация «Equip Uz»"
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("user_id", "first_name", "last_name", "phone_number")
+    list_display = ("user_id", "name", "username", "phone_number")
     list_display_links = ("user_id",)
     change_form_template = 'admin/change_form.html'
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 @admin.register(Category)
